3a7074f258964d938373324960c093e0d222f65e
joshn28 <jono2td8@gmail.com>
2021-09-17 17:06:07 -0400

    Allow user to decide how big the grid should be after clicking button

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
-const container = document.querySelector('#container');
+let container = document.querySelector('#container');
 const clearButton = document.querySelector('button');
+const body = document.querySelector('body');
 
-function createGrid() {
-    for (let i = 0; i < 16 * 16; i++) {
+function createGrid(cellSize = 16) {
+    container.style['grid-template-columns'] = `repeat(${cellSize}, 1fr)`;
+    container.style['grid-template-rows'] = `repeat(${cellSize}, 1fr)`;
+    for (let i = 0; i < cellSize ** 2; i++) {
         const squareDiv = document.createElement('div');
         squareDiv.style.backgroundColor = "lightgrey";
         squareDiv.textContent = " ";
@@ -21,4 +24,14 @@ clearButton.addEventListener('click', () => {
     squareDivs.forEach((div) => {
         div.style.backgroundColor = "lightgrey";
     });
+    let numberofSquares = Number(prompt('Enter the number of squares per side for the new grid'));
+    console.log(numberofSquares);
+    if (numberofSquares <= 100) {
+        console.log('worked');
+        container.remove();
+        container = document.createElement('div');
+        container.setAttribute('id', 'container');
+        body.appendChild(container);
+        createGrid(numberofSquares);
+    }
 });
