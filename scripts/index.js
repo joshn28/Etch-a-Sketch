614e7e3ebfb99f1f31e907ec9bbb9f68b95b5b95
joshn28 <jono2td8@gmail.com>
2021-09-17 16:31:45 -0400

    Added button with an event listener to clear grid

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,24 @@
 const container = document.querySelector('#container');
+const clearButton = document.querySelector('button');
 
 function createGrid() {
     for (let i = 0; i < 16 * 16; i++) {
         const squareDiv = document.createElement('div');
-        squareDiv.style.backgroundColor = "green";
-        squareDiv.style.width = '100%';
-        squareDiv.style.height = '100%';
-        squareDiv.textContent = i;
+        squareDiv.style.backgroundColor = "lightgrey";
+        squareDiv.textContent = " ";
+        squareDiv.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = "black";
+        });
         container.appendChild(squareDiv);
     }
 }
 
 createGrid();
+
+const squareDivs = document.querySelectorAll('#container div');
+
+clearButton.addEventListener('click', () => {
+    squareDivs.forEach((div) => {
+        div.style.backgroundColor = "lightgrey";
+    });
+});
